be62c6ea65b5eeb17de659822737f7ca8664f091
gokaydmrl <gokaydemirel96@gmail.com>
2022-09-10 13:18:21 +0300

    mongodbConnection

---
+++ middleware/db.js
@@ -0,0 +1,29 @@
+import { MongoClient } from "mongodb";
+import nextConnect from "next-connect";
+
+const { default: mongoose } = require("mongoose");
+
+const client = new MongoClient(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function database(req, res, next) {
+  try {
+    // console.log("cl", client);
+    /* if (!client.isConnected()) await client.connect(); */
+    req.dbClient = client;
+    req.db = client.db("news");
+    await client.connect();
+
+    return next();
+  } catch (error) {
+    console.log("dber", error);
+  }
+}
+
+const middleware = nextConnect();
+
+middleware.use(database);
+
+export default middleware;
